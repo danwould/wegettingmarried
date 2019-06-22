b700ed5e7d1f2e319941f4a0fe81740e5b65dda3
Dan Wood <dan_wood@apple.com>
2019-06-21 23:33:42 -0700

    Fix capitalization

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Footer = () => (
-  <footer className = "footer">
+  <footer className="footer">
     <a href = "http://danwoulddesign.com" target="_blank" rel="noopener noreferrer">&copy; Dan Would Designs</a>
   </footer>
 )
