8188eff05280eeb10ccbc7d050ec12f7091c5cb5
Dan Wood <dan_wood@apple.com>
2019-07-05 12:32:27 -0700

    Dangerously set innerhtml to make muted attr work

--- src/components/video.js
+++ src/components/video.js
@@ -2,17 +2,18 @@ import React from 'react'
 import videoUrl from '../video/wedding-3.mp4'
 
 const Video = () => (
-  <div className='player-wrapper'>
-    <video
-      autoPlay
-      loop
-      muted
-      playsInline
-      className='react-player'
-      width='100%'
-      height='100%'>
-      <source src={videoUrl} type="video/mp4"/>
-    </video>
+  <div className='player-wrapper' dangerouslySetInnerHTML={{ __html: `
+        <video
+          loop
+          muted
+          autoplay
+          playsinline
+          src="${videoUrl}"
+          className='react-player'
+          width='100%'
+          height='100%'
+        />,
+      ` }}>
   </div>
 )
 
